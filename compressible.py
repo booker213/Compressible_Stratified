89a3e97bc84ea818ca78963f6b5c6e4abba4a404
booker213 <will.booker@hotmail.co.uk>
2016-03-15 11:26:40 +0000

    Check Poisson bracket for errors, seems to be currently correct.

--- compressible.py
+++ compressible.py
@@ -2,24 +2,23 @@ from firedrake import *
 
 
 
-# Needed to enable re-evaluation of the energy
-# Disables Firedrake caching
-
 
 #Create Mesh
+# Chosen mesh is a squrare with m square elements.
 m = 16
 mesh = UnitSquareMesh(m, m,  quadrilateral=quadrilateral)
-order_basis = 3 
+order_basis = 0 
 
 # Note x[0] = z
 #      x[1] = x
-# Attempt to make an easy extension to 3D ?
+# In effect we have stratified the fluid in the x direction 
+# instead of the z direction. This is fine as long as gravity 
+# acts in the z direction.
 
 
 
 
 # Declare timestep 
-# Currently replicating Sanders' work
 timestep = 1./16.
 
 #Define Constants
@@ -32,7 +31,7 @@ theta = Constant(0.5) # Alternating Flux
 
 
 #Define Function Spaces
-
+# and create mixed function space
 V = VectorFunctionSpace(mesh, "DG", order_basis) # Dim of VFS = dim mesh, unless specified otherwise.
 R = FunctionSpace(mesh, "DG", order_basis)
 P = FunctionSpace(mesh, "DG", order_basis)
@@ -49,6 +48,7 @@ dr_0 = Function(R)
 r_0.interpolate(r_0_expression)
 dr_0.interpolate(dr_0_expression)
 
+# Output background density to check orientation
 
 backgrounddensity_file = File('./Results/background_density.pvd')
 backgrounddensity_file << r_0
@@ -62,7 +62,7 @@ backgrounddensity_file << r_0
 w_ = Function(W)
 (u_, r_, p_) = split(w_)
 w = Function(W)
-#(u, r, p) = split(w)
+
 
 #Set up element normal
 n = FacetNormal(mesh)
@@ -90,34 +90,23 @@ n = FacetNormal(mesh)
 #Current approach does not work
 #Externally determine dispersion relation and insert as constant may be appropiate for now
 
-# Matlab value
+# Matlab value for wave frequency
 omega =  8.900185996715988
 
-#Energy is not as expected.
-# Currently need rechecking.
-#Interpolating now works
 
-#exact_u = Expression( " exp( -0.5*( N + 1 )*x[0] )* ( 2 * pi/( 4 * pi*pi - omega*omega)) * ( -2*pi*cos(2*pi*x[0]) - 0.5*(N-1)*sin(2*pi*x[0]) )* sin(2*pi*x[1])*sin (omega  + 0.1)", N = 2, omega =  8.900185996715988 )
-
-#exact_w =  Expression( " exp( -0.5*( N + 1 )*x[0] )* sin(2*pi*x[0])*cos(2*pi*x[1])*sin(omega  + 0.1)", N = 2, omega =  8.900185996715988  )
+#Define initial conditions
+(u_, r_, p_) = w_.split()
 
 exact_r = Expression( " exp( -0.5*( N + 1 )*x[0] )*(omega / ( 4*pi*pi - omega*omega))*( ( 0.5*(N + 1) - (4*pi*pi*N/(omega*omega)))*sin(2*pi*x[0]) - 2*pi*cos(2*pi*x[0]))*cos(2*pi*x[1])*cos(0.1) " , N = 2, omega =  8.900185996715988)
 
 exact_p = Expression( " exp( -0.5*( N + 1 )*x[0] )*(omega / ( 4*pi*pi - omega*omega))*( -0.5*(N-1)*sin(2*pi*x[0]) - 2*pi *cos(2* pi *x[0]))*cos(2*pi*x[1])*cos( 0.1) ", N = 2, omega =  8.900185996715988)
-
-(u_, r_, p_) = w_.split()
-
-#(u, r, p) = w.split()
-
-#Define initial conditions
 
 r_.interpolate(exact_r)
 p_.interpolate(exact_p)
 u_.interpolate(Expression([" exp( -0.5*( N + 1 )*x[0] )* sin(2*pi*x[0])*cos(2*pi*x[1])*sin( 0.1)",  " exp( -0.5*( N + 1 )*x[0] )* ( 2 * pi/( 4 * pi*pi - omega*omega)) * ( -2*pi*cos(2*pi*x[0]) - 0.5*(N-1)*sin(2*pi*x[0]) )* sin(2*pi*x[1])*sin ( 0.1)"], N = 2, omega =  8.900185996715988))
 
 ## u_.interpolate(Expression([component_0, component_1]))
-# Lawrence fix
-# Interpolation to indexed pieces of a VFS is not currently implemented.
+
 
 #Initial Energy
 E0 = assemble( (0.5*inner((u_),(u_))/r_0 + 0.5*pow(g,2)*pow(( r_ - p_/c_0),2)/(r_0*N) + 0.5* pow(p_,2)/(r_0*c_0))*dx )
@@ -127,33 +116,37 @@ E0 = assemble( (0.5*inner((u_),(u_))/r_0 + 0.5*pow(g,2)*pow(( r_ - p_/c_0),2)/(r
 # a =   ( u*dFdu_vec + r*dFdr + p* dFdp)*dx - 0.5*dt*Poisson_Bracket( u , r , p )
 # L =   ( u_*dFdu_vec + r_*dFdr + p_* dFdp)*dx + 0.5*dt*Poisson_Bracket( u_ , r_ , p_ )
 
+
+
+
 #Define Poisson Bracket
 #Possibly as a python definition
 
 a0 = (dot(u, dFdu_vec) + r*dFdr + p*dFdp)*dx
-a1 = (- dot (grad((g*g)/(N*N)*(r - p/c_0)), dFdu_vec) + dot(grad(r_0*dFdr), u))*dx
+a1 = (- dot (grad((g*g)/(N*r_0)*(r - p/c_0)), dFdu_vec) + dot(grad(r_0*dFdr), u))*dx
 a2 = ( dr_0*(((g*g)/(r_0*N))*(r - p/c_0)*dFdu_vec[0] - dFdr*u[0]))*dx
 a3 = (g*r_0*(dFdp*u[0] - ( (g*g)/(r_0*N)*(p/(c_0*c_0)-r/c_0)+p/(r_0*c_0) )*dFdu_vec[0] ) )*dx
-a4 = (- dot (grad(( (g*g*c_0*c_0)/(N)*(p/(c_0*c_0)-r/c_0)+p/(c_0) )), dFdu_vec) + dot(grad(c_0*r_0*dFdp), u))*dx
-a5 = ( -jump((g*g)/(N*N)*(r - p/c_0))*dot((1-theta)*dFdu_vec('-')+ theta*dFdu_vec('+'), n('-')))*dS
+a4 = (- dot (grad(( (g*g*c_0)/(N)*(p/(c_0*c_0)-r/c_0)+p/(c_0) )), dFdu_vec) + dot(grad(c_0*r_0*dFdp), u))*dx
+a5 = ( -jump((g*g)/(N*r_0)*(r - p/c_0))*dot((1-theta)*dFdu_vec('-')+ theta*dFdu_vec('+'), n('-')))*dS
 a6 = ( jump(r_0*dFdr)*dot((1-theta)*u('-')+ theta*u('+'), n('-')))*dS
-a7 = ( -jump(( (g*g*c_0*c_0)/(N)*(p/(c_0*c_0)-r/c_0)+p/(c_0) ))*dot((1-theta)*dFdu_vec('-')+ theta*dFdu_vec('+'), n('-')))*dS
+a7 = ( -jump( (g*g*c_0)/(N)*(p/(c_0*c_0)-r/c_0)+p/(c_0) )*dot((1-theta)*dFdu_vec('-')+ theta*dFdu_vec('+'), n('-')))*dS
 a8 = ( jump(c_0*r_0*dFdp)*dot((1-theta)*u('-')+ theta*u('+'), n('-')))*dS
 
 a = a0 - 0.5*timestep*( a1 + a2 +  a3 + a4 + a5 + a6 + a7 + a8)
 
 L0 = (dot(u_, dFdu_vec) + r_*dFdr + p_*dFdp)*dx
-L1 = (- dot (grad((g*g)/(N*N)*(r_ - p_/c_0)), dFdu_vec) + dot(grad(r_0*dFdr), u_))*dx
+L1 = (- dot (grad((g*g)/(N*r_0)*(r_ - p_/c_0)), dFdu_vec) + dot(grad(r_0*dFdr), u_))*dx
 L2 = ( dr_0*(((g*g)/(r_0*N))*(r_ - p_/c_0)*dFdu_vec[0] - dFdr*u_[0]))*dx
 L3 = (g*r_0*(dFdp*u_[0] - ( (g*g)/(r_0*N)*(p_/(c_0*c_0)-r_/c_0)+p_/(r_0*c_0) )*dFdu_vec[0] ) )*dx
-L4 = (- dot (grad(( (g*g*c_0*c_0)/(N)*(p_/(c_0*c_0)-r_/c_0)+p_/(c_0) )), dFdu_vec) + dot(grad(c_0*r_0*dFdp), u_))*dx
-L5 = ( -jump((g*g)/(N*N)*(r_ - p_/c_0))*dot((1-theta)*dFdu_vec('-')+ theta*dFdu_vec('+'), n('-')))*dS
+L4 = (- dot (grad(( (g*g*c_0)/(N)*(p_/(c_0*c_0)-r_/c_0)+p_/(c_0) )), dFdu_vec) + dot(grad(c_0*r_0*dFdp), u_))*dx
+L5 = ( -jump((g*g)/(N*r_0)*(r_ - p_/c_0))*dot((1-theta)*dFdu_vec('-')+ theta*dFdu_vec('+'), n('-')))*dS
 L6 = ( jump(r_0*dFdr)*dot((1-theta)*u_('-')+ theta*u_('+'), n('-')))*dS
-L7 = ( -jump(( (g*g*c_0*c_0)/(N)*(p_/(c_0*c_0)-r_/c_0)+p_/(c_0) ))*dot((1-theta)*dFdu_vec('-')+ theta*dFdu_vec('+'), n('-')))*dS
+L7 = ( -jump( (g*g*c_0)/(N)*(p_/(c_0*c_0)-r_/c_0)+p_/(c_0) )*dot((1-theta)*dFdu_vec('-')+ theta*dFdu_vec('+'), n('-')))*dS
 L8 = ( jump(c_0*r_0*dFdp)*dot((1-theta)*u_('-')+ theta*u_('+'), n('-')))*dS
 
 L = L0 + 0.5*timestep*( L1 + L2 + L3 + L4 + L5 + L6 + L7 + L8)
 
+# Note that we have no boundary surface terms as n.dh/du = n.df/du = 0 at the boundaries
 
 
 # visualisation files
@@ -162,16 +155,16 @@ density_file = File('./Results/density.pvd')
 pressure_file = File('./Results/pressure.pvd')
 
 # output initial conditions
-# output does not currently work
-# issue must be the initial conditions 
+
 density_file << r_
 pressure_file << p_
 u_file << u_
 
 
 out=Function(W)
+# Setup time conditions
 t = 0.0
-end = 20.
+end = 1.
 
 while (t < end):
  t+=timestep
@@ -189,7 +182,7 @@ while (t < end):
 
  #Assemble Energy
  E = assemble( (0.5*inner((u),(u))/r_0 + 0.5*pow(g,2)*pow(( r - p/c_0),2)/(r_0*N) + 0.5* pow(p,2)/(r_0*c_0))*dx )
-
+ # Print time and energy drift, drift should be around machine precision.
  print t, abs((E-E0)/E0)
 
 
