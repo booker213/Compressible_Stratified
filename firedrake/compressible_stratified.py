7cd5f32870b70bee2bbdf7c74e58679e9f69b6f3
booker213 <will.booker@hotmail.co.uk>
2016-05-05 12:00:38 +0100

    Tested convergence. Error conservation tolerance appears to depend upon basis. Accuracy still good for linear (worst energy case)

--- firedrake/compressible_stratified.py
+++ firedrake/compressible_stratified.py
@@ -8,7 +8,7 @@ from firedrake import *
 Nx = 16
 Nz = 16
 mesh = UnitSquareMesh(Nx, Nz,  quadrilateral=quadrilateral)
-order_basis = 0 
+order_basis = 4
 
 # Note x[0] = z
 #      x[1] = x
@@ -22,7 +22,7 @@ t = 0.0
 end = 1.
 
 # Declare timestep 
-dt = 1./16.
+dt = 1./32.
 
 #Define Constants
 c_0 = Constant(1.0) # Speed of sound
@@ -95,13 +95,13 @@ omega =  8.900185996715988
 #Define initial conditions
 (u0, rho0, p0) = w_.split()
 
-exact_rho = Expression( " exp( -0.5*( N + 1 )*x[0] )*(omega / ( 4*pi*pi - omega*omega))*( ( 0.5*(N + 1) - (4*pi*pi*N/(omega*omega)))*sin(2*pi*x[0]) - 2*pi*cos(2*pi*x[0]))*cos(2*pi*x[1])*cos(0.1) " , N = 2, omega =  8.900185996715988)
+exact_rho = Expression( " exp( -0.5*( N + 1 )*x[0] )*(omega / ( 4*pi*pi - omega*omega))*( ( 0.5*(N + 1) - (4*pi*pi*N/(omega*omega)))*sin(2*pi*x[0]) - 2*pi*cos(2*pi*x[0]))*cos(2*pi*x[1])*cos(0.125) " , N = 2, omega =  8.900185996715988)
 
-exact_p = Expression( " exp( -0.5*( N + 1 )*x[0] )*(omega / ( 4*pi*pi - omega*omega))*( -0.5*(N-1)*sin(2*pi*x[0]) - 2*pi *cos(2* pi *x[0]))*cos(2*pi*x[1])*cos( 0.1) ", N = 2, omega =  8.900185996715988)
+exact_p = Expression( " exp( -0.5*( N + 1 )*x[0] )*(omega / ( 4*pi*pi - omega*omega))*( -0.5*(N-1)*sin(2*pi*x[0]) - 2*pi *cos(2* pi *x[0]))*cos(2*pi*x[1])*cos( 0.125) ", N = 2, omega =  8.900185996715988)
 
 rho0.interpolate(exact_rho)
 p0.interpolate(exact_p)
-u0.interpolate(Expression([" exp( -0.5*( N + 1 )*x[0] )* sin(2*pi*x[0])*cos(2*pi*x[1])*sin( 0.1)",  " exp( -0.5*( N + 1 )*x[0] )* ( 2 * pi/( 4 * pi*pi - omega*omega)) * ( -2*pi*cos(2*pi*x[0]) - 0.5*(N-1)*sin(2*pi*x[0]) )* sin(2*pi*x[1])*sin ( 0.1)"], N = 2, omega =  8.900185996715988))
+u0.interpolate(Expression([" exp( -0.5*( N + 1 )*x[0] )* sin(2*pi*x[0])*cos(2*pi*x[1])*sin( 0.125)",  " exp( -0.5*( N + 1 )*x[0] )* ( 2 * pi/( 4 * pi*pi - omega*omega)) * ( -2*pi*cos(2*pi*x[0]) - 0.5*(N-1)*sin(2*pi*x[0]) )* sin(2*pi*x[1])*sin ( 0.125)"], N = 2, omega =  8.900185996715988))
 
 ## u_.interpolate(Expression([component_0, component_1]))
 
@@ -176,28 +176,44 @@ out=Function(W)
 
 
 while (t < end):
- t+=dt
+    t+=dt
  
- solve(a == L, out, solver_parameters={'ksp_rtol': 1e-15})
- u, rho, p = out.split()
+    solve(a == L, out, solver_parameters={'ksp_rtol': 1e-15})
+    u, rho, p = out.split()
 
- # Assign appropriate name in results file
- u.rename("Velocity")
- rho.rename("Density")
- p.rename("Pressure")
+    # Assign appropriate name in results file
+    u.rename("Velocity")
+    rho.rename("Density")
+    p.rename("Pressure")
  
- # Output results
- outfile.write(u, rho,p, time =t)
+    # Output results
+    outfile.write(u, rho,p, time =t)
  
- # Assign output as previous timestep for next time update
- u0.assign(u)
- rho0.assign(rho)
- p0.assign(p)
- #Assemble Energy
- E = assemble( ((inner(u,u)/r_0 + (rho**2 - 2*rho*p/c_0 + (p/c_0)**2)/(r_0*N) + (p**2)/(r_0*c_0) )*0.5)*dx )
- E_file.write('%-10s %-10s\n' % (t,abs((E-E0)/E0)))
- # Print time and energy drift, drift should be around machine precision.
- print t, abs((E-E0)/E0)
+    # Assign output as previous timestep for next time update
+    u0.assign(u)
+    rho0.assign(rho)
+    p0.assign(p)
+
+    #Assemble Energy
+    E = assemble( ((inner(u,u)/r_0 + (rho**2 - 2*rho*p/c_0 + (p/c_0)**2)/(r_0*N) + (p**2)/(r_0*c_0) )*0.5)*dx )
+    E_file.write('%-10s %-10s\n' % (t,abs((E-E0)/E0)))
+
+    # Print time and energy drift, drift should be around machine precision.
+    print "At time %g, energy drift is %g" % (t, E-E0)
+
+
+
+
+
+
+# Create analytic solutions for error analysis
+exact_rho= Function(R)
+exact_rho.interpolate(Expression( " exp( -0.5*( N + 1 )*x[0] )*(omega / ( 4*pi*pi - omega*omega))*( ( 0.5*(N + 1) - (4*pi*pi*N/(omega*omega)))*sin(2*pi*x[0]) - 2*pi*cos(2*pi*x[0]))*cos(2*pi*x[1])*cos(omega*t+0.125) " , N = 2, omega =  8.900185996715988, t = t))
+
+
+# Print error for density
+error_rho = errornorm(rho, exact_rho,  norm_type='L2', degree_rise=4)
+print "At time %g, l2 error in density is %g" % (t, error_rho)
 # Close energy write
 E_file.close()
 
